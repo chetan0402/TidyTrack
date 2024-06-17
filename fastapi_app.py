5041402f11bf6c2d595150b244bdfdc3b74cac03
Chetan0402 <chetansharma67g@gmail.com>
2024-06-17 12:10:20 +0530

    internet/get fix

--- fastapi_app.py
+++ fastapi_app.py
@@ -97,8 +97,10 @@ def sweeperReport(sweeper_report: SweeperReport, db: Session = Depends(get_db)):
 
 
 @app.get("/internet/get", tags=["webview"])
-def internetGet(location: Union[str, None], from_time: int, to_time: int, offset: int, db: Session = Depends(get_db)):
+def internetGet(request: Request, location: Union[str, None], from_time: int, to_time: int, offset: int,
+                db: Session = Depends(get_db)):
     return template.TemplateResponse(name="dashboard_entry_show.html", context={
+        "request": request,
         "data": getReport(db, location, from_time, to_time, offset=offset,
                           report_type=ReportType.INTERNET)})
 
