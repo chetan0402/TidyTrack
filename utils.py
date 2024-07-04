ec7c1840cd524f704074879ab1cb3d8b15227fb5
Chetan0402 <chetansharma67g@gmail.com>
2024-07-04 15:26:26 +0530

    more detail added

--- utils.py
+++ utils.py
@@ -85,7 +85,7 @@ def verifyGroup(user: models.Userbase, group_to_verify: int):
 def addReport(db: Session, report_element: Union[BaseReport, WithImgReport],
               report_type: constants.ReportType.ReportType) -> Message:
     if not validUUID(report_element.id):
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid UUID")
     user = getUserFromToken(db, report_element.token)
     verifyGroup(user, 0)
 
