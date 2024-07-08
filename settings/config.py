3c5d215a0d431482bd4af179c5409d0624350982
Chetan0402 <chetansharma67g@gmail.com>
2024-07-08 11:21:29 +0530

    everything migrated to config

--- settings/config.py
+++ settings/config.py
@@ -11,11 +11,25 @@ class Config(BaseModel):
     LATEST_APP_PATH: str
     TARGET_PHOTO_SIZE: int
     VERSION_FILE: str
+    SQLALCHEMY_DATABASE_URL: str
     loaded: bool = False
     debug: bool = False
 
 
+config_obj = None
+
+
 def get_config() -> Config:
-    with open(Path.home().joinpath("settings").joinpath("config.json"), "r") as file:
-        config = json.load(file)
-        return Config(**config)
+    global config_obj
+    if config_obj is None:
+        with open(Path.home().joinpath("settings").joinpath("config.json"), "r") as file:
+            config_obj = json.load(file)
+            return Config(**config_obj)
+    else:
+        return Config(**config_obj)
+
+
+def reload_config() -> Config:
+    global config_obj
+    config_obj = None
+    return get_config()
